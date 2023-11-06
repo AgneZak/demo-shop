563668c7068d4cda2138159a9a175241802344fc
Agne Zakaite <azakaite@gmail.com>
2023-11-06 18:50:32 +0200

    Updates for layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,8 @@
 <template>
     <v-layout class="rounded rounded-md">
-        <v-app-bar title="Application bar"></v-app-bar>
-        <v-main> Main Content </v-main>
+        <v-app-bar title="Shop"></v-app-bar>
+        <v-navigation-drawer permanent></v-navigation-drawer>
+        <v-main> <slot /> </v-main>
         <v-footer></v-footer>
     </v-layout>
 </template>
---
+++ layouts/login.vue
@@ -0,0 +1,6 @@
+<template>
+    <v-layout class="rounded rounded-md">
+        <v-app-bar title="Login"></v-app-bar>
+        <v-main> <slot /></v-main>
+    </v-layout>
+</template>
