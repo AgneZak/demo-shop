b64eea1e7c479202a3e15b4f21880b318ec84cfb
Agne Zakaite <azakaite@gmail.com>
2023-11-06 17:09:42 +0200

    Add initial layout

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <v-layout class="rounded rounded-md">
+        <v-app-bar title="Application bar"></v-app-bar>
+        <v-main> Main Content </v-main>
+        <v-footer></v-footer>
+    </v-layout>
+</template>
+<script setup></script>
