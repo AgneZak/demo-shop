287bd75413f940cb9b3f43674b1ba5ee2c2381b2
Agne Zakaite <azakaite@gmail.com>
2023-11-12 18:52:27 +0200

    Update users table

--- pages/users.vue
+++ pages/users.vue
@@ -2,28 +2,34 @@
     <div>
         <section>
             <h1>Users</h1>
-            <v-text-field v-model="limit" name="limit" label="Limit" type="text" placeholder="limit"></v-text-field>
+            <div class="d-flex">
+                <v-text-field v-model="limit" name="limit" label="Limit" type="text" placeholder="limit" variant="outlined"></v-text-field>
 
-            <v-radio-group v-model="sort" inline label="Sort">
-                <v-radio label="DESC" value="desc"></v-radio>
-                <v-radio label="ASC" value="asc"></v-radio>
-            </v-radio-group>
-
+                <v-radio-group v-model="sort" inline label="Sort">
+                    <v-radio label="DESC" value="desc"></v-radio>
+                    <v-radio label="ASC" value="asc"></v-radio>
+                </v-radio-group>
+            </div>
             <v-btn class="mt-4" color="primary" @click="loadUsers">Get Users</v-btn>
         </section>
-        <v-table density="compact" fixed-header height="300px">
+        <v-table density="compact" fixed-header height="calc(54vh - 40px)">
             <thead>
                 <tr>
                     <th class="text-left">ID</th>
                     <th class="text-left">Name</th>
                     <th class="text-left">Email</th>
+                    <th class="text-left">Edit</th>
                 </tr>
             </thead>
             <tbody>
+                <tr v-if="usersStore.users.length <= 0">
+                    <td>Select props and press Get Users</td>
+                </tr>
                 <tr v-for="user in usersStore.users" :key="user.id">
                     <td>{{ user.id }}</td>
                     <td>{{ user.name.firstname }}</td>
                     <td>{{ user.email }}</td>
+                    <td><v-icon icon="mdi-pen"></v-icon></td>
                 </tr>
             </tbody>
         </v-table>
