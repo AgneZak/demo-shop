75b84b89386aa182a74d9439778c85ebe96db71a
Agne Zakaite <azakaite@gmail.com>
2023-11-12 19:45:42 +0200

    Add initial user modal

---
+++ components/users-modal.vue
@@ -0,0 +1,65 @@
+<template>
+    <v-row justify="center">
+        <v-dialog v-model="dialog" persistent width="1024">
+            <v-card>
+                <v-card-title>
+                    <span class="text-h5">User Profile</span>
+                </v-card-title>
+                <v-card-text>
+                    <v-container>
+                        <v-row>
+                            <v-col cols="12" sm="6" md="4">
+                                <v-text-field label="Legal first name*" required></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                                <v-text-field
+                                    label="Username*"
+                                    hint="example of persistent helper text"
+                                    persistent-hint
+                                    required
+                                ></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-text-field label="Email*" required></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-text-field label="Password*" type="password" required></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6">
+                                <v-autocomplete
+                                    :items="['Vilnius', 'Kaunas', 'Klaipeda', 'Siauliai', 'Alytus']"
+                                    label="City"
+                                    multiple
+                                ></v-autocomplete>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                    <small>*indicates required field</small>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue-darken-1" variant="text" @click="close"> Close </v-btn>
+                    <v-btn color="blue-darken-1" variant="text" @click="close"> Save </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-row>
+</template>
+<script lang="ts" setup>
+    const props = defineProps<{
+        show: boolean;
+    }>();
+
+    const dialog = computed(() => props.show);
+
+    const emit = defineEmits<{
+        close: [close: boolean];
+    }>();
+
+    function close() {
+        emit('close', false);
+    }
+</script>
